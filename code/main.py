6cf1a16df9e829f6844e1ee69ae241cf708becd2
Bridger Fisher <laxfish@bridgers-mbp.devices.wcslc.net>
2018-02-22 20:25:40 -0700

    [Release 1] Update: Allows going through multiple questions.

--- code/main.py
+++ code/main.py
@@ -8,44 +8,48 @@ import MySQLdb
 
 #!/usr/bin/python
 import MySQLdb
-
-# db = MySQLdb.connect(host="localhost",  # your host
-#                      user="admin",   # username
-#                      passwd="admin",        # password
-#                      db="MathSportsDB") # name of the database
-
-db = pymysql.connect("localhost", "admin", "admin", "MathSportsDB")
-
-#initializing app
 app = Flask(__name__)
+db = pymysql.connect("localhost", "admin", "admin", "MathSportsDB")
+question_number = 1
 
 @app.route('/')
 def question():
+    global question_number
     # Create a Cursor object to execute queries.
     cur = db.cursor()
-    #query = ("SELECT FIRST FROM students ")
     # Select data from table using SQL query.
-    cur.execute("SELECT question, answer FROM question WHERE question_id = 1;")
-    #cur.execute(query)
+    query = "SELECT question, answer FROM question WHERE question_id = {};".format(question_number)
+    cur.execute(query)
 
     questions=[question[0] for question in cur.description] #return headers with values
     data = cur.fetchall()
     data_list=[]
     for element in data:
         data_list.append(dict(zip(questions,element)))
-	return render_template('question.html', data=data_list)
+	return render_template('question.html', data=data_list, question_number=question_number)
 
 @app.route('/checkAnswer/', methods=['POST'])
 def check_answer():
+        global question_number
 	student_answer = request.form["answer"]
-	print student_answer
-	if(student_answer == '8'):					#make a query from the database here for the answer.
+        cur = db.cursor()
+        query = "SELECT question, answer FROM question WHERE question_id = {};".format(question_number)
+        cur.execute(query)
+        questions=[question[0] for question in cur.description] #return headers with values
+        data = cur.fetchall()
+        data_list=[]
+        for element in data:
+            data_list.append(dict(zip(questions,element)))
+        correct_answer = data_list[0]["answer"]
+        print 'correct_answer', type(correct_answer)
+        print 'student answer', type(student_answer)
+        question_number += 1
+	if(int(student_answer) == int(correct_answer)):					#make a query from the database here for the answer.
 		answer = 'True';
-		return render_template('check_answer.html', answer=answer)
+		return render_template('check_answer.html', answer=answer, data=data_list, question_number=question_number)
 	else:
 		answer = 'False';
-		return render_template('check_answer.html', answer=answer)
-
+		return render_template('check_answer.html', answer=answer, data=data_list, question_number=question_number)
 # @app.route('/<string:page_name>/')
 # def static_page(page_name):
 #     return render_template('%s.html' % page_name)
