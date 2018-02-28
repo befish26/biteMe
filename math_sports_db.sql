9e7e842758a711b14483325a8d46884b8d5e3f0e
Ian Sime <Iansime@Vickis-MacBook-Pro-2.local>
2018-02-27 19:06:25 -0700

    Demo database queries

--- math_sports_db.sql
+++ math_sports_db.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS QUESTION (
 
 INSERT INTO QUESTION(question, answer) VALUES ('4+4=', 8);
 INSERT INTO QUESTION(question, answer) VALUES ('2+2=', 4);
+INSERT INTO QUESTION(question, answer) VALUES ('23+22=', 45);
+INSERT INTO QUESTION(question, answer) VALUES ('60+31=', 91);
+INSERT INTO QUESTION(question, answer) VALUES ('9+8=', 17);
+INSERT INTO QUESTION(question, answer) VALUES ('9-3=', 6);
+INSERT INTO QUESTION(question, answer) VALUES ('45-25=', 20);
+INSERT INTO QUESTION(question, answer) VALUES ('99-50=', 49);
+INSERT INTO QUESTION(question, answer) VALUES ('34-27=', 7);
+INSERT INTO QUESTION(question, answer) VALUES ('15-8=', 7);
