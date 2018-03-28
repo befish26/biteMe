4126ab496641ecf5ce78b6c9ac31db96cc1fa911
befish26 <laxfish@bridgers-mbp.devices.wcslc.net>
2018-03-27 19:37:25 -0600

    [Release 2] Student_profiles.

---
+++ queries.sql
@@ -0,0 +1,11 @@
+select student_progress.student_id, student_progress.total_percent_correct, student_questions.question_id  from stude
+nt_progress inner join student_questions on student_progress.student_id = student_questions.student_id;
+
+UPDATE student_questions set response = "correct" where student_id = 1 AND question_id = 1;
+
+select student_progress.student_id, student_questions.response, student_questions.question_id  from student_progress
+inner join student_questions on student_progress.student_id = student_questions.student_id;
+
+
+SELECT COUNT(RESPONSE) FROM STUDENT_QUESTIONS WHERE response = "correct";
+select count(*) from student_questions;
